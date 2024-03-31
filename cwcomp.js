4a32ae4d2e56eda8b61aaed2e4d72c61fab56c04
Phil Hanna <ph1204@gmail.com>
2024-03-31 16:51:02 -0400

    Handle login

--- cwcomp.js
+++ cwcomp.js
@@ -1,120 +1,127 @@
-      $(document).ready(function () {
-        $("#mozart").click(create_menus);
+const RESTHost = "localhost";
+const RESTPort = 5053;
+const RESTURL = `http://${RESTHost}:${RESTPort}`;
+const RESTURL_LOGIN = `${RESTURL}/login`;
 
-        //  ===========================================================
-        //  create_help_menu: Creates the "help" menu
-        //  ===========================================================
+function do_login() {
+  alert("Entering do_login");
+}
 
-        function create_help_menu() {
-          $("#menubar").append(
-            '<a class="w3-bar-item w3-button w3-right" href="https://github.com/philhanna/crossword/wiki">Help</a>'
-          );
-        }
+$(document).ready(function () {
+  $("#mozart").click(create_menus);
 
-        //==============================================================
-        //  create_menu: Creates a menu on the menu bar.
-        //
-        //  Parameters: mnu - a JSON object described below
-        //
-        //  mnu is a JSON object consisting of:
-        //
-        //      "name"      the menu label
-        //      "items"     an array of mnuItem objects for this menu
-        //
-        //  mnuItem is a JSON object consisting of:
-        //
-        //      "id"        the ID of the menu item
-        //      "label"     the menu item name
-        //      "enabled"   true or false
-        //==============================================================
+  //  ===========================================================
+  //  create_help_menu: Creates the "help" menu
+  //  ===========================================================
 
-        function create_menu(mnu) {
+  function create_help_menu() {
+    $("#menubar").append(
+      '<a class="w3-bar-item w3-button w3-right" href="https://github.com/philhanna/crossword/wiki">Help</a>'
+    );
+  }
 
-          let name = mnu.name;
-          let items = mnu.items;
+  //==============================================================
+  //  create_menu: Creates a menu on the menu bar.
+  //
+  //  Parameters: mnu - a JSON object described below
+  //
+  //  mnu is a JSON object consisting of:
+  //
+  //      "name"      the menu label
+  //      "items"     an array of mnuItem objects for this menu
+  //
+  //  mnuItem is a JSON object consisting of:
+  //
+  //      "id"        the ID of the menu item
+  //      "label"     the menu item name
+  //      "enabled"   true or false
+  //==============================================================
 
-          // Create the menu element (elem)
-          let elem = document.createElement("div");
-          elem.setAttribute("class", "w3-dropdown-hover w3-blue-gray");
+  function create_menu(mnu) {
+    let name = mnu.name;
+    let items = mnu.items;
 
-          // Create the menu name button (elemButton)
-          let elemButton = document.createElement("button");          
-          elemButton.setAttribute("id", `btn${name}`);
-          elemButton.setAttribute("class", "w3-button");
+    // Create the menu element (elem)
+    let elem = document.createElement("div");
+    elem.setAttribute("class", "w3-dropdown-hover w3-blue-gray");
 
-          // Create the text of the menu name button (text)
-          let text = document.createTextNode(`${name}`);
-          elemButton.appendChild(text);
-          elem.appendChild(elemButton);
+    // Create the menu name button (elemButton)
+    let elemButton = document.createElement("button");
+    elemButton.setAttribute("id", `btn${name}`);
+    elemButton.setAttribute("class", "w3-button");
 
-          // Create the dropdown menu div
-          let elemDrop = document.createElement("div");
-          elemDrop.setAttribute("id", `drop${name}`);
-          elemDrop.setAttribute(
-            "class",
-            "w3-dropdown-content w3-bar-block w3-card-4"
-          );
-          elem.appendChild(elemDrop);
+    // Create the text of the menu name button (text)
+    let text = document.createTextNode(`${name}`);
+    elemButton.appendChild(text);
+    elem.appendChild(elemButton);
 
-          // Add the menu items
+    // Create the dropdown menu div
+    let elemDrop = document.createElement("div");
+    elemDrop.setAttribute("id", `drop${name}`);
+    elemDrop.setAttribute(
+      "class",
+      "w3-dropdown-content w3-bar-block w3-card-4"
+    );
+    elem.appendChild(elemDrop);
 
-          for (var i = 0; i < items.length; i++) {
-            let mnuItem = items[i];
-            let elem = create_menu_item(mnuItem);
-            elemDrop.appendChild(elem);
-          }
+    // Add the menu items
 
-          return elem;
-        }
+    for (var i = 0; i < items.length; i++) {
+      let mnuItem = items[i];
+      let elem = create_menu_item(mnuItem);
+      elemDrop.appendChild(elem);
+    }
 
-        //==============================================================
-        //  create_menu_item: Creates a single menu item.
-        //
-        //  Parameters: mnuItem - a JSON object described below
-        //
-        //  mnuItem is a JSON object consisting of:
-        //
-        //      "id"        the ID of the menu item
-        //      "label"     the menu item name
-        //      "enabled"   true or false
-        //==============================================================
+    return elem;
+  }
 
-        function create_menu_item(mnuItem) {
-          let id = mnuItem.id;
-          let label = mnuItem.label;
-          let enabled = mnuItem.enabled;
+  //==============================================================
+  //  create_menu_item: Creates a single menu item.
+  //
+  //  Parameters: mnuItem - a JSON object described below
+  //
+  //  mnuItem is a JSON object consisting of:
+  //
+  //      "id"        the ID of the menu item
+  //      "label"     the menu item name
+  //      "enabled"   true or false
+  //==============================================================
 
-          let elem = document.createElement("a");
-          elem.setAttribute("id", id);
-          var classNames = "w3-bar-item w3-button";
-          if (enabled == false) {
-            classNames += " w3-disabled";
-          }
-          elem.setAttribute("class", classNames);
+  function create_menu_item(mnuItem) {
+    let id = mnuItem.id;
+    let label = mnuItem.label;
+    let enabled = mnuItem.enabled;
 
-          let text = document.createTextNode(label);
-          elem.appendChild(text);
+    let elem = document.createElement("a");
+    elem.setAttribute("id", id);
+    var classNames = "w3-bar-item w3-button";
+    if (enabled == false) {
+      classNames += " w3-disabled";
+    }
+    elem.setAttribute("class", classNames);
 
-          return elem;
-        }
+    let text = document.createTextNode(label);
+    elem.appendChild(text);
 
-        //==============================================================
-        //  create_menus: Creates the menus from the menu.json object
-        //==============================================================
+    return elem;
+  }
 
-        function create_menus() {
+  //==============================================================
+  //  create_menus: Creates the menus from the menu.json object
+  //==============================================================
 
-          // Everything but the "help" menu
-          $.getJSON("menu.json", function (data) {
-            let root = data["menudata"];
-            for (var i = 0; i < root.length; i++) {
-              let mnu = root[i];
-              let elemMnu = create_menu(mnu);
-              $("#menubar").append(elemMnu);
-            }
-          });
+  function create_menus() {
+    // Everything but the "help" menu
+    $.getJSON("menu.json", function (data) {
+      let root = data["menudata"];
+      for (var i = 0; i < root.length; i++) {
+        let mnu = root[i];
+        let elemMnu = create_menu(mnu);
+        $("#menubar").append(elemMnu);
+      }
+    });
 
-          // The "help" menu
-          create_help_menu();
-        }
-      });
+    // The "help" menu
+    create_help_menu();
+  }
+});
